a514fdc667aacd3e7b783847a7534ac691706b64
Yuriy Lipchey <xaosmmm@gmail.com>
2021-06-06 20:34:04 +0300

    feat: add project files

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+import './App.scss';
+
+function App(): ReactElement {
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
